965b5408c173f71ce104e1d23dacfaa80d47b81b
gwongeonyeong <guan5308@gmail.com>
2021-05-28 14:22:40 +0900

    battery를 ID로 구분해 2개 배터리 로그 저장

--- zetabot_main/scripts/battery_log.py
+++ zetabot_main/scripts/battery_log.py
@@ -1,4 +1,3 @@
-
 #! /usr/bin/env python
 
 import rospy
@@ -9,35 +8,35 @@ import sys, tty, select, termios, os
 from zetabot_main.msg import BatteryInformationMsgs
 battery1 = BatteryInformationMsgs()
 battery2 = BatteryInformationMsgs()
-
-def battery1_callback(msg) :
+log_directory = "/home/zetabank/robot_log/battery_log"
+def battery_callback(msg) :
     global battery1
-    battery1 = msg
-
-def battery2_callback(msg) :
     global battery2
-    battery2 = msg
-
+    if msg.id == 0x60 :
+        battery1 = msg
+    else :
+        battery2 = msg
 
 
 def main():
 
     rospy.init_node("batt_log")
 
-    batt1_sub = rospy.Subscriber("/battery1",BatteryInformationMsgs,battery1_callback)
-    batt1_sub = rospy.Subscriber("/battery2",BatteryInformationMsgs,battery2_callback)
+    batt_sub = rospy.Subscriber("/battery",BatteryInformationMsgs,battery_callback)
     today = time.strftime('%Y_%m_%d', time.localtime(time.time()))
 
     minit = 99
 
     rospy.sleep(1)
 
-    file_name = "/home/zetabank/robot_log/battery_log/batt_log_"+today+".csv"
+    file_name = log_directory + "/batt_log_"+today+".csv"
 
 
     if os.path.isfile(file_name):
         print('ok')
     else :
+        if not os.path.exists(log_directory):
+            os.makedirs(log_directory)
         f = open(file_name,'w')
         wr = csv.writer(f)
         log_name = ['Time'] + [j + i for i in BatteryInformationMsgs.__slots__  for j in ['BAT1_','BAT2_']]
@@ -49,7 +48,7 @@ def main():
 
     
     try :
-        while True :
+        while (rospy.is_shutdown) :
 
 
             while time.localtime(time.time()).tm_min == minit :
@@ -57,7 +56,7 @@ def main():
 
             f = open(file_name,'a')
             wr = csv.writer(f)
-            log_name = ['Time'] + [j + i for i in BatteryInformationMsgs.__slots__  for j in ['BAT1_','BAT2_']]
+            log_name = ['Time'] + [j + i for i in BatteryInformationMsgs.__slots__[1:]  for j in ['BAT1_','BAT2_']]
 
 
             now_time = str(time.localtime(time.time()).tm_hour) + ":" + str(time.localtime(time.time()).tm_min)
@@ -74,19 +73,20 @@ def main():
 
             minit = time.localtime(time.time()).tm_min
 
+        # rospy.spin()
+
+        print("done")
 
 
-    except KeyboardInterrupt :
+
+    except:
         print("exit")
-        sys.exit()
-
-
-    rospy.spin()
 
         # except Exception:
         #     print("unknown error")
         
     print("Done")
+
 
 if __name__ == "__main__" :
     main()
