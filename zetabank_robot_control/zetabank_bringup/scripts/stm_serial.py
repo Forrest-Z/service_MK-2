1a2bca678073f5ceaeae1a4c1da92044f30054c6
gwongeonyeong <guan5308@gmail.com>
2021-06-10 10:23:43 +0900

    stm_serial 토픽 체크후 자동 연결

--- zetabank_robot_control/zetabank_bringup/scripts/stm_serial.py
+++ zetabank_robot_control/zetabank_bringup/scripts/stm_serial.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import rospy
-import os
+import os,sys,threading, time
+from sensor_msgs.msg import Imu
 
-rospy.init_node("stm_starter")
+class Ros_check:
+    def __init__(self, imu_topic):
+        self.check_flag = False
+        self.imu_sub = rospy.Subscriber(imu_topic, Imu, self.callback)
+    def callback(self, msg):
+        self.check_flag = True
 
-os.system("rosrun rosserial_python serial_node.py _baud:=460800 _port:=/dev/$(ls -lrt /dev | grep ^l | grep stlinkv2-1 |grep ttyACM | awk '{print$(NF-2)}') __name:='stm'")
+class Command:
+    def __init__(self):
+        self.command_rosrun = "rosrun rosserial_python serial_node.py _port:=/dev/ttyACM{} _baud:=460800 __name:='STM'"
+    def ACM_check(self):
+        for i in range(2):
+            command_rosrun = self.command_rosrun.format(i)
+            t1 = threading.Thread(target= ACM_thread, name= "ACM thread", args=(command_rosrun,))
+            t1.setDaemon(True)
+            t1.start()
+            #ros
+            imu_topic = '/imu'
+            ros_check = Ros_check(imu_topic)
+            time.sleep(5)
+
+            if not ros_check.check_flag:
+                nodes = os.popen("rosnode lsit").readline()
+                os.system("rosnode kill /STM")
+                t1.join()
+            else:
+    
+                t1.join()
+                
+
+def ACM_thread(command_rosrun):
+    os.system(command_rosrun)
+
+
+if __name__=='__main__':
+    rospy.init_node("stm_starter")
+    Command().ACM_check()
