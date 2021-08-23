78b5bc5634ecb903da7205d76ab3f9e6bbf2c908
hoonium <hah8266@gmail.com>
2021-08-23 17:09:52 +0900

    stm_serial.py, sonar_filter.py : imu 연결 끊겼을때 재연결 srv 실행

--- zetabank_robot_control/zetabank_bringup/scripts/stm_serial.py
+++ zetabank_robot_control/zetabank_bringup/scripts/stm_serial.py
@@ -4,6 +4,8 @@
 import rospy
 import os,sys,threading, time
 from sensor_msgs.msg import Imu
+from std_msgs.msg import String
+from std_srvs.srv import Empty, EmptyResponse, EmptyRequest
 
 class Ros_check:
     def __init__(self, imu_topic):
@@ -13,31 +15,66 @@ class Ros_check:
         self.check_flag = True
 
 class Command:
-    def __init__(self):
+    
+    def __init__(self):    
         self.command_rosrun = "rosrun rosserial_python serial_node.py _port:=/dev/ttyACM{} _baud:=115200 __name:='STM'"
-    def ACM_check(self):
-        for i in range(2):
-            command_rosrun = self.command_rosrun.format(i)
-            t1 = threading.Thread(target= ACM_thread, name= "ACM thread", args=(command_rosrun,))
-            t1.setDaemon(True)
-            t1.start()
-            #ros
-            imu_topic = '/imu'
-            ros_check = Ros_check(imu_topic)
-            time.sleep(10)
+        self.number = 99
+        
+        self.imu_error_service = rospy.Service('/emergency_stm', Empty, self.ACM_check)
 
-            if not ros_check.check_flag:
-                nodes = os.popen("rosnode lsit").readline()
-                os.system("rosnode kill /STM")
-                t1.join()
+    def ACM_check(self,msg = EmptyRequest()):
+        time.sleep(0.2)
+        os.system('rosnode kill /STM')
+        while True:          
+            lsusb = self.STM_usb()
+            if lsusb:
+                print('STM_on')
+                if self.number != 99: 
+                    time.sleep(2)
+                    self.ACM_double_check(self.number)
+                    return EmptyResponse()
+
+                else :
+                    for i in range(2):        
+                        t1 = self.ACM_double_check(i)
+                    #ros
+                        imu_topic = '/imu'
+                        ros_check = Ros_check(imu_topic)
+                        time.sleep(10)
+
+                        if not ros_check.check_flag:
+                            nodes = os.popen("rosnode list").readline()
+                            os.system("rosnode kill /STM")
+                            t1.join()
+                        else:
+                            self.number = i       
+                            break  
+                break  
             else:
-                break
-                
+                pass
+                # return EmptyResponse()
 
-def ACM_thread(command_rosrun):
-    os.system(command_rosrun)
+    def ACM_double_check(self,number): 
+        command_rosrun = self.command_rosrun.format(number)
+        t1 = threading.Thread(target= self.ACM_thread, name="ACM thread", args=(command_rosrun,))
+        t1.setDaemon(True)
+        t1.start()
+        return t1   
 
+    def STM_usb(self):
+        lsusb_list = os.popen("lsusb").read()
+        if 'STM' in lsusb_list:
+            return True
+        else:
+            return False
+
+    def ACM_thread(self,command_rosrun):
+        os.system(command_rosrun)
 
 if __name__=='__main__':
     rospy.init_node("stm_starter")
-    Command().ACM_check()
+    acm_command = Command()
+
+    acm_command.ACM_check()
+
+    rospy.spin()
--- zetabot_main/scripts/sonar_filter.py
+++ zetabot_main/scripts/sonar_filter.py
@@ -13,7 +13,7 @@ import time
 import full_coverage.msg
 from std_msgs.msg import UInt8
 from std_msgs.msg import Bool
-
+from std_srvs.srv import Empty
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import Range
@@ -24,6 +24,7 @@ from std_msgs.msg import String
 from full_coverage.msg import lidar_filter 
 from zetabot_main.srv import ModuleControllerSrv
 from zetabot_main.msg import SonarArray
+from sensor_msgs.msg import Imu
 
 
 sonar_warning_top = 40
@@ -45,10 +46,17 @@ cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 bumper_flag = False
 estop_flag = False
 MVStop_flag = False
+imu_avr_term = None
+imu_get_list = []
+imu_get_time = None
+stm_called = True
+stm_recall_flag = False
+
+
 
 emergency_topic = "/emergency_stop"
 emergency_pub = rospy.Publisher(emergency_topic,String, queue_size=10)
-
+emergency_call = rospy.ServiceProxy("/emergency_stm", Empty)
 module_controller_srv = rospy.ServiceProxy("/module_controller_srv",ModuleControllerSrv)
 
 rospy.init_node('sonar_filter')
@@ -101,10 +109,34 @@ def recv_MBStop(data):
     global MVStop_flag
     MVStop_flag = data.data
 
+def recv_imu(data):
+    global imu_avr_term
+    global imu_get_list
+    global imu_get_time
+    global stm_called
+    global stm_recall_flag
+    stm_called = True
+    stm_recall_flag = False
+
+
+    if imu_avr_term == None :
+        imu_get_list.append(rospy.get_time())
+        if len(imu_get_list) >= 5 :
+            imu_avr_term = (imu_get_list[-1] - imu_get_list[0]) / 4
+            imu_get_time = imu_get_list[-1]
+
+    else :
+        imu_get_time = rospy.get_time()
+
+def emergency_stm_call():
+    emergency_call()
+
 def emergency_send () :
     global bumper_flag
     global estop_flag
-
+    global stm_called
+    global stm_recall_flag
+    
     while True:
         rospy.sleep(0.01)
         emergency_msg = ""
@@ -132,6 +164,32 @@ def emergency_send () :
         if MVStop_flag == True :
             emergency_msg += "/move_base_stop"
 
+        if imu_avr_term != None :
+            print(rospy.get_time() - imu_get_time)
+            print(imu_avr_term)
+            print(imu_avr_term + (imu_avr_term/2))
+            print(imu_get_list)
+        if imu_avr_term == None :
+            emergency_msg += "/imu_error_stop"
+
+
+        elif rospy.get_time() - imu_get_time >= (imu_avr_term*10) :
+            # os.system("rosnode kill /stm_starter")
+            emergency_msg += "/imu_error_stop"
+            stm_called = False
+
+        if not stm_called and not stm_recall_flag:
+            stm_recall_flag = True
+            print("recall")
+            #stm call thread
+            
+            #set_param odom_sub.msg
+
+            t1 = threading.Thread(target=emergency_stm_call)
+            t1.daemon = True 
+            t1.start()
+
+            print("return")
         # if (sonar_sensor.sonar_UF <= sonar_stop_top or sonar_sensor.sonar_DF <= sonar_stop_bottom  ) :
         #     emergency_msg += "/sonar_stop"
         #     print("sonar_sensor_val :",sonar_sensor.sonar_UF)
@@ -161,6 +219,9 @@ def main():
 
     move_base_stop_topic = "/move_base_stop"
     rospy.Subscriber(move_base_stop_topic,Bool,recv_MBStop)
+
+    imu_topic = "/imu"
+    rospy.Subscriber(imu_topic,Imu,recv_imu)
     
     rospy.spin()
 
